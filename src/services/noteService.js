0bf9663576aece137dc10965b35c1842728335cb
Yanti <wsutr@hotmail.com>
2023-04-14 21:00:01 -0600

    add proxy to backend

--- src/services/noteService.js
+++ src/services/noteService.js
@@ -16,10 +16,5 @@ const update = (id, newObject) =>{
     return request.then(response => response.data)
 }
 
-// export default { 
-//     getAll: getAll, 
-//     create: create, 
-//     update: update 
-//   }
 const allServices = { getAll, create, update}
 export default allServices
